b90468d62215548c99bb394b3663e307a79ccf3b
Пользователь <kulanbaev.bolot11gmail.com>
2022-06-07 11:39:11 +0600

    added post request

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ GET - ПОЛУЧЕНИЕ ДАННЫХ
 JSON-SERVER -W DB.JSON -P 8000
 
 */
+
+/*
+CRUD - Create(post) Read(get) Update(put/patch) Delete(delete)
+*/
+
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById("inp-add");
+let btnAdd = document.getElementById("btn-add");
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener("click", function () {
+  let newTodo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+});
